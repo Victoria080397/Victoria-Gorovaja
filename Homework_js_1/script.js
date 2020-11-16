231e0cddba703df1d9be2bed64495be9dcf1dea3
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-11-16 06:46:51 +0200

    Update script.js

--- Homework_js_1/script.js
+++ Homework_js_1/script.js
@@ -20,37 +20,37 @@
 
 //Написати фунцію аргументом  якої буде масив чисел і ця функція буде повертати середнє арифметичне цих чисел;
 
-//function Averg(a) {
-//    let sum = 0;
-//    for (i = 0; i < a.length; i++) {
-//        sum += a[i];
-//    }
-//    sum /= a.length;
-//    return sum;
-//}
-//let avr = Averg([3, 17, 8, 89]);
-//console.log(avr);
+function Averg(a) {
+    let sum = 0;
+    for (i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+    sum /= a.length;
+    return sum;
+}
+let avr = Averg([10, 5, 20, 15]);
+console.log(avr);
 
 
 
 
 
-//Написати фунцію аргументом  якої буде масив цілих чисел і ця функція буде повертати таке значення (сума всіх парних чисел       відняти         сума всіх непарних);
+//Написати фунцію аргументом якої буде масив цілих чисел і ця функція буде повертати таке значення(сума всіх парних чисел відняти сума всіх непарних);
 
-//function Summ(a) {
-//    let sum1 = 0;
-//    let sum2 = 0;
-//    for (let i = 0; i < a.length; i++) {
-//        if (a[i] % 2 == 0) {
-//            sum1 += a[i];
-//        } else if (a[i] % 2 == 1) {
-//            sum2 += a[i];
-//        }
-//    }
-//    return sum1 - sum2;
-//}
-//
-//console.log(Summ([1, 8, 24, 5, 36]));
+function Summ(a) {
+    let sum1 = 0;
+    let sum2 = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 2 == 0) {
+            sum1 += a[i];
+        } else if (a[i] % 2 == 1) {
+            sum2 += a[i];
+        }
+    }
+    return sum1 - sum2;
+}
+
+console.log(Summ([1, 8, 24, 5, 36]));
 
 
 
@@ -58,16 +58,16 @@
 
 //Написати фунцію аргументами  якої будуть два масиви цілих чисел і ця функція буде повертати масив зі спільних елементів( Наприлад вхідні масиви - [1,2,3,4,5] , [1,4,8]. Функція має повернути - [1,4] )
 
-//function CommonEl(a, b) {
-//    let arr = [];
-//    for (i = 0; i < a.length; i++) {
-//        for (i = 0; i < b.length; i++) {
-//            if (a[i] == b[i]) {
-//               arr.push(a[i] && b[i]); 
-//            }  
-//        }
-//    }
-//    return arr;
-//}
-//let com = CommonEl([3, 5, 7, 9], [1, 5, 8, 9]);
-//console.log(com);
+function CommonEl(a, b) {
+    let arr = [];
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            if (a[i] == b[j]) {
+                arr.push(a[i] && b[j]);
+            }
+        }
+    }
+    return arr;
+}
+let com = CommonEl([3, 5, 7, 9], [1, 5, 8, 9]);
+console.log(com);
