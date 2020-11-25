0c7006397098f572cfef56fb7a2ff646a08d81fe
Victoria080397 <54357147+Victoria080397@users.noreply.github.com>
2020-11-25 19:34:28 +0200

    Update Homework_js_slider

---
+++ Homework_js_slider/script.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    paddingLeft: '50px',
+    paddingRight: '50px',
+//    variableWidth: true,
+//    adaptiveHeight: true,
+
+    //    fade: true,
+    //    cssEase: 'linear',
+    nextArrow: '<span class="next"></span>',
+    prevArrow: '<span class="prev"></span>'
+});
