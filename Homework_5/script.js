e2e1ad8cf6524cc858888315d8eb624b5367ed50
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-12-04 14:07:20 +0200

    slick

--- Homework_5/script.js
+++ Homework_5/script.js
@@ -2,6 +2,10 @@ $('.slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow: '<span class="next"></span>',
-    prevArrow: '<span class="prev"></span>'
+    arrows:false,
+//    nextArrow: '<span class="next"></span>',
+//    prevArrow: '<span class="prev"></span>'
+});
+$('.next').on('click',function(){
+    $('.slider').slick('slickPrev');
 });
