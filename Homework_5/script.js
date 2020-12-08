b44787737bf46ed9c9a304572633491dbf425847
Victoria080397 <54357147+Victoria080397@users.noreply.github.com>
2020-12-08 19:01:48 +0200

    update

--- Homework_5/script.js
+++ Homework_5/script.js
@@ -2,10 +2,15 @@ $('.slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows:false,
-//    nextArrow: '<span class="next"></span>',
-//    prevArrow: '<span class="prev"></span>'
+    arrows: false,
+    //    nextArrow: '<span class="next"></span>',
+    //    prevArrow: '<span class="prev"></span>'
 });
-$('.next').on('click',function(){
+$('.next').on('click', function () {
+    $('.slider').slick('slickNext');
+
+});
+$('.prev').on('click', function () {
     $('.slider').slick('slickPrev');
+
 });
